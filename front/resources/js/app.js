9505f3c49975f8597ef2dd66bf9676bd1ccb03c6
Stan Rares Alexandru <stan.rares@gmail.com>
2016-05-30 23:14:08 +0300

    Corrected app.js

--- front/resources/js/app.js
+++ front/resources/js/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var app = angular.module('Travlr', ['ngRoute', 'ngMaterial', 'ngCookies']);
 
 app.config(function($routeProvider, $mdThemingProvider) {
@@ -31,37 +30,3 @@ app.config(function($routeProvider, $mdThemingProvider) {
 		.primaryPalette('amber')
 		.accentPalette('blue');
 });
-=======
-var app = angular.module('Travlr', ['ngRoute', 'ngMaterial','materialCalendar']);
-
-app.config(function($routeProvider, $mdThemingProvider) {
-	$routeProvider
-		.when('/', {
-			controller: 'HomeController',
-    		templateUrl: 'views/pages/home.html'
-		})
-		.when('/flights/:id', {
-			controller: 'FlightController',
-			templateUrl: 'views/pages/flight.html'
-		})
-		.when('/search/:dep/:arr/:startDate/:returnDate?/:numberAdults?/:numberChildren?/:numberInfants?/:priceMax?/:durationMax?', {
-			controller: 'SearchController',
-			templateUrl: 'views/pages/search.html'
-		})
-		.when('/group', {
-			controller: 'GroupController',
-			templateUrl: 'views/pages/group.html'
-		})
-		.when('/cart', {
-			controller: 'CartController',
-			templateUrl: 'views/pages/cart.html'
-		})
-		.otherwise({
-			redirectTo: '/'
-		});
-
-	$mdThemingProvider.theme('default')
-		.primaryPalette('amber')
-		.accentPalette('blue');
-});
->>>>>>> origin/master

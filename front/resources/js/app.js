624b534977aba25a0de1858d8fde2b32f5b876d0
Stan Rares Alexandru <stan.rares@gmail.com>
2016-05-30 23:10:50 +0300

    Add files via upload

---
+++ front/resources/js/app.js
@@ -0,0 +1,32 @@
+var app = angular.module('Travlr', ['ngRoute', 'ngMaterial','materialCalendar']);
+
+app.config(function($routeProvider, $mdThemingProvider) {
+	$routeProvider
+		.when('/', {
+			controller: 'HomeController',
+    		templateUrl: 'views/pages/home.html'
+		})
+		.when('/flights/:id', {
+			controller: 'FlightController',
+			templateUrl: 'views/pages/flight.html'
+		})
+		.when('/search/:dep/:arr/:startDate/:returnDate?/:numberAdults?/:numberChildren?/:numberInfants?/:priceMax?/:durationMax?', {
+			controller: 'SearchController',
+			templateUrl: 'views/pages/search.html'
+		})
+		.when('/group', {
+			controller: 'GroupController',
+			templateUrl: 'views/pages/group.html'
+		})
+		.when('/cart', {
+			controller: 'CartController',
+			templateUrl: 'views/pages/cart.html'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+
+	$mdThemingProvider.theme('default')
+		.primaryPalette('amber')
+		.accentPalette('blue');
+});
